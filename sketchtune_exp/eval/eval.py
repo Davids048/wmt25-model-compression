4212f8cde5819c18907e15bf9933f7a7dd528771
David Su <js202@terminator8.cs.rice.edu>
2025-07-03 10:24:52 -0500

    Refactor eval.

--- sketchtune_exp/eval/eval.py
+++ sketchtune_exp/eval/eval.py
@@ -17,10 +17,68 @@ from utils.log import setup_logging
 from utils.model_utils import create_hf_model, load_hf_tokenizer
 from utils.utils import set_random_seed
 
-
+DEBUG_TEST_SIZE = 100
 
 
 LOG = logging.getLogger(__name__)
+
+def setup_model_tokenizer(
+    model_path:str,
+    model_quantizer_path:str,
+    finetuned_quantizer_path:str,
+    device
+):
+    tokenizer = load_hf_tokenizer(
+        model_path,
+        fast_tokenizer=True
+    )
+    tokenizer.padding_side = 'left'
+    LOG.info(f"Tokenizer padding: {tokenizer.padding_side}")
+
+
+    model = create_hf_model(
+        AutoModelForCausalLM,
+        model_path,
+        tokenizer
+    )
+    with open(model_quantizer_path, 'rb') as file:
+        quantizers = pickle.load(file) 
+    with open(os.path.join(finetuned_quantizer_path, 'quant_grids.pkl'), 'rb') as f:
+        quant_grids = pickle.load(f)
+    replace_layers(model, quantizers, quant_grids)
+    model = model.to(device)
+    model.eval()
+    return model, tokenizer
+
+def generate(model, tokenizer, prompts, batch_size, device):
+    LOG.info("Start generation >>>>>>>>>>")
+    generation_config = GenerationConfig(
+        temperature=0.1,
+        top_p=0.75,
+        top_k=40,
+        num_beams=4,
+        pad_token_id=model.config.pad_token_id,
+        eos_token_id=model.config.eos_token_id,
+        bos_token_id=model.config.bos_token_id,
+    )
+    model_outputs = [] 
+    outputs = generate_completions(
+        model=model,
+        device=device,
+        tokenizer=tokenizer,
+        prompts=prompts,
+        max_new_tokens=256,                          
+        batch_size=batch_size,
+        stop_id_sequences=[[tokenizer.eos_token]],
+        verbose=False,
+        generation_config = generation_config
+    )
+    model_outputs.extend(outputs)
+    LOG.info("End generation <<<<<<<<<<<<<")
+
+    return model_outputs
+
+
 
 
 def main():
@@ -50,64 +108,37 @@ def main():
     LOG.info(f"Configs:\n{model_config}\n{eval_config}\n{data_config}")
 
 
-    # Load Tokenizer 
-    tokenizer = load_hf_tokenizer(
-        model_config.model_path,
-        fast_tokenizer=True
+    model, tokenizer = setup_model_tokenizer(
+        model_config.model_path, 
+        model_config.model_quantizer_path,
+        eval_config.finetuned_quantizer_path,
+        device=device
     )
-    tokenizer.padding_side = 'left'
-    LOG.info(f"Tokenizer padding: {tokenizer.padding_side}")
 
-    # Load Model 
-    model = create_hf_model(
-        AutoModelForCausalLM,
-        model_config.model_path,
-        tokenizer
-    )
-    with open(model_config.model_quantizer_path, 'rb') as file:
-        quantizers = pickle.load(file) 
-    with open(os.path.join(eval_config.finetuned_quantizer_path, 'quant_grids.pkl'), 'rb') as f:
-        quant_grids = pickle.load(f)
-    replace_layers(model, quantizers, quant_grids)
-    model = model.to(device)
-    model.eval()
 
     # prep test data
-    test_data = create_test_dataset(
-        data_config.data_path, 
-        data_config.source_lang, 
-        data_config.target_lang
-    )
     if args.debug:
-        test_data = test_data[:100] if len(test_data) >=100 else test_data
+        test_data = create_test_dataset(
+            data_config.data_path, 
+            data_config.source_lang, 
+            data_config.target_lang,
+            limit=DEBUG_TEST_SIZE,
+        )
         LOG.info(f"Using small test size for debugging, size: ({len(test_data)})")
-
-
-    # Create generation 
-    LOG.info("Start generation >>>>>>>>>>")
-    generation_config = GenerationConfig(
-        temperature=0.1,
-        top_p=0.75,
-        top_k=40,
-        num_beams=4,
-        pad_token_id=model.config.pad_token_id,
-        eos_token_id=model.config.eos_token_id,
-        bos_token_id=model.config.bos_token_id,
+    else:
+        test_data = create_test_dataset(
+            data_config.data_path, 
+            data_config.source_lang, 
+            data_config.target_lang,
+        )
+        
+    model_outputs = generate(
+        model,
+        tokenizer,
+        test_data,
+        eval_config.per_device_eval_batch_size,
+        device=device
     )
-    model_outputs = [] 
-    outputs = generate_completions(
-        model=model,
-        device=device,
-        tokenizer=tokenizer,
-        prompts=test_data,
-        max_new_tokens=256,                          
-        batch_size=eval_config.per_device_eval_batch_size,
-        stop_id_sequences=[[tokenizer.eos_token]],
-        verbose=False,
-        generation_config = generation_config
-    )
-    model_outputs.extend(outputs)
-    LOG.info("End generation <<<<<<<<<<<<<")
 
     # Write outputs 
     with open(os.path.join(output_dir, "output.txt"), 'w') as f:
@@ -115,8 +146,6 @@ def main():
             f.write(f"{line.rstrip()}\n")
 
     LOG.info("Finished writing outputs to file.")
-
-    
 
 if __name__ == "__main__":
     main()
